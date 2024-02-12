5fb7404e110feb886100d64a9881fce10c17b954
Quinost <Quinost@users.noreply.github.com>
2024-02-12 19:18:57 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls';
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ 
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule ,
+    RouterOutlet,
+    CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'val';
+  showDance: boolean = true;
+  showOnlyYes: boolean = false;
+  onBtnClick() {
+    this.showDance = false;
+  }
+
+  onNoBtnClick() {
+    window.alert("Błąd");
+    this.showOnlyYes = true;
+  }
 }
